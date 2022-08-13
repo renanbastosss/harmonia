5ba20b749cb9d93d6aa6e80be8f2baad37f2efe1
Renan Bastos <renan_bastos@hotmail.com>
2022-08-13 14:20:51 -0300

    Versão 2.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,7 @@
 const menuItens = document.querySelectorAll('.menus')
 
-menuItens.forEach(item => {
-    item.addEventListener('click', function (event) {
-        const elementId = event.target.id
+const goToSection = (event) => {
+    const elementId = event.target.id
         const toSection = document.querySelector(`${elementId}`).offsetTop
 
         if (mobileScreen) {
@@ -12,14 +11,17 @@ menuItens.forEach(item => {
             })
         } else {
             window.scroll({
-                top: toSection - 60,
+                top: toSection,
                 behavior: "smooth",
             });
         }
 
         const itemSelected = document.getElementById(`${elementId}`)
         itemSelected.classList.add('active')
-    })
+}
+
+menuItens.forEach(item => {
+    item.addEventListener('click', goToSection)
 });
 
 
@@ -51,81 +53,130 @@ function changeMenuItem() {
         location.classList.add('active');
     }
 
-    if (scroll > 2080) {
+    if (scroll > 1890) {
         document.querySelector('.active').classList.remove('active');
         contacts.classList.add('active');
     }
 }
 
+
+const header = document.querySelector('header');
+let lastScrollTop = 0;
+
+const checkScroll = () => {
+    let scrollTop = window.scrollY
+
+    if (scrollTop > lastScrollTop) {
+        header.style.top = '-60px'
+    } else {
+        header.style.top = '0px'
+    }
+
+    lastScrollTop = scrollTop;
+};
+
 window.addEventListener('scroll', function () {
     changeMenuItem();
-    showButtonGoTop()
+    showButtonGoTop();
+    checkScroll();
 })
 
+//------Código para "Área do aluno indisponível"-------//
 
-const areaAluno = document.querySelector('.area-aluno');
-const modalAlert = document.querySelector('.modal-background');
+// const areaAluno = document.querySelector('.area-aluno');
+// const modalAlert = document.querySelector('.modal-background');
 
-areaAluno.addEventListener('click', (event) => {
-    event.preventDefault();
-    modalAlert.style.display = 'block';
-})
+// areaAluno.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     modalAlert.style.display = 'block';
+// })
 
-const buttonModal = document.querySelector('.modal-button');
-buttonModal.addEventListener('click', closeModalAlert);
+// const buttonModal = document.querySelector('.modal-button');
+// buttonModal.addEventListener('click', closeModalAlert);
 
-modalAlert.addEventListener('click', closeModalAlert);
+// modalAlert.addEventListener('click', closeModalAlert);
 
-function closeModalAlert() {
-    modalAlert.style.display = 'none';
-}
-
-
-
-const gallery = document.querySelectorAll('.photos img');
-const modalPhotos = document.querySelector('.background-modal');
-const photoContainer = document.querySelector('.container-imagem img')
-const closerPhoto = document.querySelector('.container-imagem span')
-
-gallery.forEach(photo => {
-    photo.addEventListener('click', () => {
-        let srcPhoto = photo.getAttribute('src');
-
-        modalPhotos.style.display = 'block';
-        photoContainer.setAttribute('src', srcPhoto)
-    });
-});
-
-function closeModal() {
-    modalPhotos.style.display = 'none';
-}
-
-modalPhotos.addEventListener('click', closeModal);
-closerPhoto.addEventListener('click', closeModal);
+// function closeModalAlert() {
+//     modalAlert.style.display = 'none';
+// }
 
 
 
-const pointer1 = document.querySelector('.pointer-1');
-const pointer2 = document.querySelector('.pointer-2');
-const pointer3 = document.querySelector('.pointer-3');
-const slider = document.querySelector('.sliders-photos')
+// const gallery = document.querySelectorAll('.photos img');
+// const modalPhotos = document.querySelector('.background-modal');
+// const photoContainer = document.querySelector('.container-imagem img')
+// const closerPhoto = document.querySelector('.container-imagem span')
 
-pointer1.addEventListener('click', () => {
-    slider.style.marginLeft = '200vw';
-    document.querySelector('.selected').classList.remove('selected');
-    pointer1.classList.add('selected')
-});
+// gallery.forEach(photo => {
+//     photo.addEventListener('click', () => {
+//         let srcPhoto = photo.getAttribute('src');
 
-pointer2.addEventListener('click', () => {
-    slider.style.marginLeft = '0vw';
-    document.querySelector('.selected').classList.remove('selected');
-    pointer2.classList.add('selected')
-});
+//         modalPhotos.style.display = 'block';
+//         photoContainer.setAttribute('src', srcPhoto)
+//     });
+// });
 
-pointer3.addEventListener('click', () => {
-    slider.style.marginLeft = '-200vw';
-    document.querySelector('.selected').classList.remove('selected');
-    pointer3.classList.add('selected')
+// function closeModal() {
+//     modalPhotos.style.display = 'none';
+// }
+
+// modalPhotos.addEventListener('click', closeModal);
+// closerPhoto.addEventListener('click', closeModal);
+
+
+
+// const pointer1 = document.querySelector('.pointer-1');
+// const pointer2 = document.querySelector('.pointer-2');
+// const pointer3 = document.querySelector('.pointer-3');
+// const slider = document.querySelector('.sliders-photos')
+
+// pointer1.addEventListener('click', () => {
+//     slider.style.marginLeft = '200vw';
+//     document.querySelector('.selected').classList.remove('selected');
+//     pointer1.classList.add('selected')
+// });
+
+// pointer2.addEventListener('click', () => {
+//     slider.style.marginLeft = '0vw';
+//     document.querySelector('.selected').classList.remove('selected');
+//     pointer2.classList.add('selected')
+// });
+
+// pointer3.addEventListener('click', () => {
+//     slider.style.marginLeft = '-200vw';
+//     document.querySelector('.selected').classList.remove('selected');
+//     pointer3.classList.add('selected')
+// });
+
+// -------------------------- GALLERY ----------------------------//
+
+const smallPhotos = document.querySelectorAll('.manual-btn');
+const firstPhoto = document.querySelector('.slide.first');
+const smallPhotosContainer = document.querySelector('.manual-navigation')
+
+smallPhotos.forEach(photo => {
+    photo.addEventListener('click', (element) => {
+        let photoSelected = element.target
+        let radioNumber = photoSelected.id.slice(3)
+        let marginSlide = radioNumber*10-10
+        let marginSmallContainer = radioNumber*120-470
+
+        firstPhoto.style.marginLeft = `-${marginSlide}%`
+
+        if (radioNumber <=3) {
+            smallPhotosContainer.style.marginLeft = '15px'
+        } else {
+            smallPhotosContainer.style.marginLeft = `-${marginSmallContainer}px`
+        }
+
+        let elementSelected = document.querySelector('.selected')
+        if (elementSelected != null) {
+            elementSelected.classList.remove('selected')
+        }
+
+        photoSelected.classList.add('selected')
+        
+    })
 });
 
 
@@ -162,7 +213,7 @@ if (mobileScreen) {
 
 const buttonGoTop = document.querySelector('.return-top');
 buttonGoTop.addEventListener('click', () => {
-    window.scroll({
+    window.scrollTo({
         top: 0,
         behavior: "smooth",
     });
@@ -177,3 +228,8 @@ function showButtonGoTop() {
         buttonGoTop.style.display = 'flex';
     }
 }
+
+
+
+
+// window.addEventListener('scroll', checkScroll);
